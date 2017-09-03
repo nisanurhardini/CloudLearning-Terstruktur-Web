e0a470ea90bae86969bd3bb3ba8ece836778b1ae
Hardiyan Ichsan Gunawan <hardiyan.gunawan@gmail.com>
2017-09-03 13:27:54 +0700

    Db_not_master
    Cuma Beberapa doang, nunggu struktur pastinya

---
+++ tubes_impal.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 03, 2017 at 08:24 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tubes_impal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banksoal`
+--
+
+CREATE TABLE `banksoal` (
+  `id` int(5) NOT NULL,
+  `kode_dosen` varchar(3) NOT NULL,
+  `nama_matkul` varchar(50) NOT NULL,
+  `soal` varchar(5000) NOT NULL,
+  `solusi` varchar(5000) DEFAULT NULL,
+  `tgl_input` date NOT NULL,
+  `tgl_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materi`
+--
+
+CREATE TABLE `materi` (
+  `kode_matkul` varchar(6) NOT NULL,
+  `nama_matkul` varchar(50) NOT NULL,
+  `kode_dosen` varchar(3) NOT NULL,
+  `pathfile` text NOT NULL,
+  `tgl_input` date NOT NULL,
+  `tgl_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banksoal`
+--
+ALTER TABLE `banksoal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `materi`
+--
+ALTER TABLE `materi`
+  ADD PRIMARY KEY (`kode_matkul`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banksoal`
+--
+ALTER TABLE `banksoal`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
